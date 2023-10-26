b56a70b52040d95b5a4e8a4cda3a45f39f9afa1c
Leo-Chaves <leonardo.2021107241@unicap.br>
2023-10-26 02:35:06 -0300

    Visualização Clientes

--- src/main/java/com/example/NorthwindProject/Controller/CustomersController.java
+++ src/main/java/com/example/NorthwindProject/Controller/CustomersController.java
@@ -6,21 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
-
 @RestController
-@RequestMapping("api/Custumers")
 @CrossOrigin("*")
+@RequestMapping("/api/Customers")
 public class CustomersController {
     @Autowired
     private ICustomersService iCustomersService;
 
-    @PostMapping("/list")
-    public ResponseEntity<List<Customer>> list(){
-        var result = iCustomersService.findAll();
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    @GetMapping("/list")
+    public ModelAndView list() {
+        List<Customer> customers = iCustomersService.findAll();
 
+        ModelAndView modelAndView = new ModelAndView("Customers"); // O nome da página HTML
+        modelAndView.addObject("customers", customers);
+
+        return modelAndView;
     }
 
 }
--- src/main/java/com/example/NorthwindProject/Controller/OrderController.java
+++ src/main/java/com/example/NorthwindProject/Controller/OrderController.java
@@ -8,10 +8,7 @@ import com.example.NorthwindProject.Service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,7 +20,7 @@ public class OrderController {
     @Autowired
     private IOrderService iorderService;
 
-    @PostMapping("/list")
+    @GetMapping("/list")
     public ResponseEntity<List<Order>> list(){
         var result = iorderService.findAllOrders();
         return new ResponseEntity<>(result, HttpStatus.OK);
